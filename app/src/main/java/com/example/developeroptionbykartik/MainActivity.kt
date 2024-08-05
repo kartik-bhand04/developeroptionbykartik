0c84963000ed6904c1e2b1ebe282703adb733b42
kartik.bhand <kartik.bhand.uts@gmail.com>
2024-08-05 23:21:53 +0530

    s

--- app/src/main/java/com/example/developeroptionbykartik/MainActivity.kt
+++ app/src/main/java/com/example/developeroptionbykartik/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.app.AlarmManager
+import android.provider.AlarmClock
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,5 +26,17 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
             startActivity(intent)
         }
+        val button1=findViewById<Button>(R.id.button_about_phone)
+            button1.setOnClickListener{
+                val intent1=Intent(Settings.ACTION_DEVICE_INFO_SETTINGS)
+//                val intent2 = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+//                    putExtra(AlarmClock.EXTRA_MESSAGE, "Set alarm")
+//                    putExtra(AlarmClock.EXTRA_HOUR, 7)
+//                    putExtra(AlarmClock.EXTRA_MINUTES, 30)
+//                }
+
+                startActivity(intent1)
+
+            }
     }
 }
